4794e3d6bff19a6d5c8b8898a721d2735b498cbb
Joachim Schwarm <joachim@schwarm.co>
2019-01-16 15:48:13 +0100

    host-aware polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,11 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+host=`uname -n`
+
 # Launch polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload $host &
   done
 else
-  polybar --reload top &
+  polybar --reload $host &
 fi
